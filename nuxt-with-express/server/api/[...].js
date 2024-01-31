16a27e3d1b176801cb248f27d42ad5af1cf0e0bf
kmjoy <152054850+kmjoy@users.noreply.github.com>
2024-01-31 10:59:24 +0900

    Add files via upload

---
+++ nuxt-with-express/server/api/[...].js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+router.get("/hello", (req, res) => {
+  res.send("Hello Express from catch-all route server Handler");
+});
+
+router.all("/json", (req, res) => {
+  res.json({ data: "some data as JSON" });
+});
+
+app.use("/api", router);
+
+// convert node middleware to h3 handlar
+// ref: https://github.com/nuxt/framework/releases/tag/v3.0.0-rc.12
+const h3handler = fromNodeMiddleware(app);
+export default h3handler;
